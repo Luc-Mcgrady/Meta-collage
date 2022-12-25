4e4177cebd84862e13f59a73bdce8db17b4c2a86
Luc Mcgrady <lucmcgrady@gmail.com>
2022-12-25 20:51:32 +0000

    Added: Candidates array

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,20 @@
 // FIN
 pub mod image_maths;
 
+use image::{DynamicImage};
+
 fn main() {
 
     let files = ["half.png", "thing.png"].map(|a| ["test/", a].join(""));
 
     let imgs = files.map(|a| image_maths::open_file(&a));
+    let averages = imgs.map(|a| image_maths::image_average(&a));
 
-    println!("{:?}", imgs.map(|a| image_maths::image_average(&a)));
+    // [[std::vec::Vec<&DynamicImage>; 255]; 3]
+    const init: std::vec::Vec<&DynamicImage> = vec![];
+    const colours: usize = 255;
+    const channel: [std::vec::Vec<&DynamicImage>; colours] = [init; colours];
+    let mut candidates = [channel; 3];
+
+    println!("{:?}",candidates);
 }
