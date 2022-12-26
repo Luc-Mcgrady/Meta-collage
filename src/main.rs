87eb6d81413c4b25af8d914cb0c1386a2d01a595
Luc Mcgrady <lucmcgrady@gmail.com>
2022-12-26 16:04:14 +0000

    Added: Itteration optimisation

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,12 @@ fn main() {
     let paths = std::fs::read_dir("./steamed-hams/").unwrap();
     println!("Loading files1...");
     let files: &Vec<String> = &paths.map(|a| a.unwrap().path().to_str().expect("Cant convert into string?").to_owned() ).collect();
+
     println!("Loading files2...");
-    let imgs: &Vec<(&String, DynamicImage)> = &files.into_iter().map(|path| (path, image_maths::open_file(path))).collect();
-    println!("Loading files3...");
-    let image_arr: &Vec<&DynamicImage> = &imgs.into_iter().map(|(a, b)| b).collect();
+    let imgs = &files.into_iter().map(|path| (path, image_maths::open_file(path)));
+
     println!("Calculating averages...");
-    let averages = &imgs.into_iter().map(|(path, image)| {
+    let averages = &imgs.to_owned().into_iter().map(|(path, image)| {
         
         let average = image_maths::image_average(&image);
         let path = [path.to_owned().to_owned(), String::from(".avg")].join("");
@@ -35,28 +35,28 @@ fn main() {
         let pickled = serde_pickle::to_value(&average).expect("Couldent pickle :(");
         std::fs::write(path, pickled.to_string()).expect("Failed to write to file");
 
-        return average;
+        return (image, average);
     }
-    ).collect::<Vec<[usize; 3]>>();
+    ).collect::<Vec<(DynamicImage,RGB)>>();
     
     println!("Creating match table...");
 
     let mut i = 0;
-    let exact: HashMap<RGB, usize> = averages.to_owned().into_iter().map(|a| {i+=1; return (a, i);}).collect();
+    let exact: HashMap<RGB, usize> = averages.to_owned().into_iter().map(|(_, a)| {i+=1; return (a, i);}).collect();
 
     println!("Processing...");
 
     const BLOCK_SIZE: u32 = 50;
 
     //for img in imgs {
-        let img = &image_arr[0];
+        let img = &averages[0].0;
         let block = &img.to_owned().to_owned().crop(0, 0, BLOCK_SIZE, BLOCK_SIZE * (&img).height() / (&img).width());
         block.save("test/Block.png").expect("Could not save block image");
 
         let index = exact[&image_maths::image_average(&block)];
-        let gotten = (&image_arr).get(index).expect("Error fetching image");
+        let gotten = (&averages).get(index).expect("Error fetching image");
 
-        gotten.save("test/Gotten.png").expect("Could not save Gotton image");
+        gotten.0.save("test/Gotten.png").expect("Could not save Gotton image");
     //}
 
 }
