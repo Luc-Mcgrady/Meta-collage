d6df7cf93c27ec2ebf402a950ded5a3c2f317c2f
Luc Mcgrady <lucmcgrady@gmail.com>
2022-12-28 22:52:47 +0000

    Added: Command line arguments

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 Takes any video and outputs a video where every frame of said video is composed of smaller images of frames of said video
 As I made this program soely to make a steamed hams video there are several notable flaws
 
-- You have to modify the source code to change the size of the collage blocks.
 - The input and output directories are fixed.
 - The .cache folder needs to be manually deleted if you change the input frames
 - The collagify function cant be called twice on different files as the cache is static.
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 mod collage;
-use std::{path::Path, fs::create_dir_all};
+use std::{path::Path, fs::create_dir, env};
 
 use collage::meta_collage;
 
 fn main() {
-    let input_dir = Path::new("./input/");
-    create_dir_all(input_dir).expect("Failed to load input directory") ;
+    let args: Vec<String> = env::args().collect();
 
-    let output_dir = Path::new("./result/");
-    std::fs::create_dir_all(output_dir).expect("The \"result\" folder could not be created");
+    if args.len() < 4 {
+        println!("{} <input_dir:path> <output_dir:path> <block_size:int>", &args[0]);
+        return
+    }
 
-    let block_size = 35;
+    let input_dir = Path::new(&args[1]);
+    if !input_dir.exists() {
+        create_dir(input_dir).expect("Failed to load input directory") ;
+    }
+
+    let output_dir = Path::new(&args[2]);
+    if !output_dir.exists() {
+        create_dir(output_dir).expect("Failed to create output directory");
+    }
+
+    let block_size: u32 = args[3].parse().expect("Not a vaild block size");
 
     meta_collage(input_dir, output_dir, block_size);
 }
